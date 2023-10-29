06f1ecbe926d8011a9019b7aa4604183f477ff50
Sneha Bhong <144727747+BhongSneha@users.noreply.github.com>
2023-10-29 09:49:55 +0530

    Update demo.py

--- Temperature Converter/demo.py
+++ Temperature Converter/demo.py
@@ -1,8 +1,1 @@
-# Input temperature in degrees Celsius
-celsius = float(input("Enter temperature in Celsius: "))
 
-# Convert Celsius to Fahrenheit
-fahrenheit = (celsius * 9/5) + 32
-
-# Display the result
-print(f"{celsius} degrees Celsius is equal to {fahrenheit} degrees Fahrenheit")
